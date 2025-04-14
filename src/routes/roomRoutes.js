c437b3277d31bc26501a03ba099cdf0b060bbee3
Muzzammil Shahid <muzza042004@gmail.com>
2025-04-15 02:05:20 +0500

    added room route and renamed files

--- src/routes/roomRoutes.js
+++ src/routes/roomRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import sql from "mssql";
+import sql, { MAX } from "mssql";
 import { userTables } from "../config/userTables.js";
 
 import authenticateToken from "../scripts/authenticateToken.js";
@@ -16,7 +16,9 @@ router.get("/", authenticateToken, authorizeUser([userTables.admin], false), asy
     const pool = await sql.connect(config);
     const result = await pool.request().query(
       `
-      SELECT * FROM TRANSACTIONS
+      SELECT R.ROOM_ID, B.BRANCH_ID, B.[LOCATION], H.HOSPITAL_NAME, R.MAX_OCCUPANCY, R.ROOM_TYPE, R.ROOM_COST_PER_NIGHT FROM ROOMS AS R
+      INNER JOIN BRANCHES AS B ON B.BRANCH_ID = R.BRANCH_ID
+      INNER JOIN HOSPITALS AS H ON H.HOSPITAL_ID = B.HOSPITAL_ID
       `
     );
     res.status(200).json(result.recordset);
@@ -30,24 +32,23 @@ router.get("/:id", authorizeUser([userTables.admin], true), async (req, res) => 
     const id = parseInt(req.params.id);
     const pool = await sql.connect(config);
     if (isNaN(id)) {
-      return res.status(400).json({ error: "INVALID USER ID" });
+      return res.status(400).json({ error: "INVALID ROOM ID" });
     }
 
     const result = await pool
       .request()
-      .input("USER_ID", sql.Int, id)
+      .input("ROOM_ID", sql.Int, id)
       .query(
-      `
-      SELECT TRANSACTION_ID, BOOKING_ID, AMOUNT_PAYABLE, MODE_OF_PAYMENT, PAYMENT_STATUS, DISCOUNT FROM TRANSACTIONS AS T
-      INNER JOIN BOOKINGS AS B ON T.BOOKING_ID = B.BOOKING_ID
-      INNER JOIN PATIENTS AS P ON P.PATIENT_ID = B.PATIENT_ID
-      INNER JOIN USERS AS U ON U.USER_ID = P.USER_ID
-      WHERE U.USER_ID = @USER_ID;
-      `
+        `
+        SELECT R.ROOM_ID, B.BRANCH_ID, B.[LOCATION], H.HOSPITAL_NAME, R.MAX_OCCUPANCY, R.ROOM_TYPE, R.ROOM_COST_PER_NIGHT FROM ROOMS AS R
+        INNER JOIN BRANCHES AS B ON B.BRANCH_ID = R.BRANCH_ID
+        INNER JOIN HOSPITALS AS H ON H.HOSPITAL_ID = B.HOSPITAL_ID
+        WHERE R.ROOM_ID = @ROOM_ID
+        `
       );
 
     if (result.recordset.length === 0)
-      return res.status(404).json({ error: "TRANSACTION NOT FOUND" });
+      return res.status(404).json({ error: "ROOM NOT FOUND" });
 
     res.status(200).json(result.recordset);
   } catch (error) {
@@ -55,58 +56,52 @@ router.get("/:id", authorizeUser([userTables.admin], true), async (req, res) => 
   }
 });
 
-router.post("/", authenticateToken, authorizeUser([userTables.admin], true), async (req, res) => {
+router.post("/", authenticateToken, authorizeUser([userTables.admin], false), async (req, res) => {
   try {
-    let { BOOKING_ID, AMOUNT_PAYABLE, MODE_OF_PAYMENT, PAYMENT_STATUS, DISCOUNT } = req.body;
-    BOOKING_ID = parseInt(BOOKING_ID);
-    AMOUNT_PAYABLE = parseFloat(AMOUNT_PAYABLE);
-    DISCOUNT = parseFloat(DISCOUNT);
+    let { BRANCH_ID, MAX_OCCUPANCY, ROOM_TYPE, ROOM_COST_PER_NIGHT} = req.body;
+    BRANCH_ID = parseInt(BRANCH_ID);
+    MAX_OCCUPANCY = parseInt(MAX_OCCUPANCY);
 
-    const columnNames = await fetchColumnNames("TRANSACTIONS");
+    const columnNames = await fetchColumnNames("ROOMS");
 
     if (!validateRequestBody(req.body, columnNames)) {
       return res.status(400).json({ error: "INVALID REQUEST BODY" });
     }
-    if (!(BOOKING_ID && AMOUNT_PAYABLE && MODE_OF_PAYMENT && PAYMENT_STATUS && DISCOUNT >= 0)) {
+    if (!(BRANCH_ID && MAX_OCCUPANCY > 0 && ROOM_TYPE && ROOM_COST_PER_NIGHT >= 0)) {
       return res.status(400).json({ error: "ALL FIELDS ARE REQUIRED" });
     }
-    if (BOOKING_ID <= 0 || AMOUNT_PAYABLE <= 0) {
+    if (isNaN(BRANCH_ID) || isNaN(MAX_OCCUPANCY)) {
       return res.status(400).json({ error: "BAD REQUEST" });
     }
 
     const pool = await sql.connect(config);
     const result = await pool
       .request()
-      .input("BOOKING_ID", sql.Int, BOOKING_ID)
-      .input("AMOUNT_PAYABLE", sql.Money, AMOUNT_PAYABLE)
-      .input("MODE_OF_PAYMENT", sql.VarChar(50), MODE_OF_PAYMENT)
-      .input("PAYMENT_STATUS", sql.VarChar(50), PAYMENT_STATUS)
-      .input("DISCOUNT", sql.Decimal(18, 2), DISCOUNT)
+      .input("BRANCH_ID", sql.Int, BRANCH_ID)
+      .input("MAX_OCCUPANCY", sql.Int, MAX_OCCUPANCY)
+      .input("ROOM_TYPE", sql.VarChar(50), ROOM_TYPE)
+      .input("ROOM_COST_PER_NIGHT", sql.Money, ROOM_COST_PER_NIGHT)
       .query(
         `
-        INSERT INTO TRANSACTIONS (BOOKING_ID, AMOUNT_PAYABLE, MODE_OF_PAYMENT, PAYMENT_STATUS, DISCOUNT)
-        VALUES (@BOOKING_ID, @AMOUNT_PAYABLE, @MODE_OF_PAYMENT, @PAYMENT_STATUS, @DISCOUNT)
+        INSERT INTO ROOMS (BRANCH_ID, MAX_OCCUPANCY, ROOM_TYPE, ROOM_COST_PER_NIGHT)
+        VALUES (@BRANCH_ID, @MAX_OCCUPANCY, @ROOM_TYPE, @ROOM_COST_PER_NIGHT)
         `
       );
 
-    
-
-    const USER_ID = null;
-    const transactionIDResult = await pool
+    const ROOM_ID = null;
+    const roomIDResult = await pool
       .request()
-      .input("USER_ID", sql.Int, USER_ID)
-      .input("BOOKING_ID", sql.Int, BOOKING_ID)
+      .input("ROOM_COST_PER_NIGHT", sql.Money, ROOM_COST_PER_NIGHT)
       .query(
         `
-        SELECT MAX(TRANSACTION_ID) AS TRANSACTION_ID FROM TRANSACTIONS
-        WHERE BOOKING_ID = @BOOKING_ID;
+        SELECT MAX(ROOM_ID) AS ROOM_ID FROM ROOMS
+        WHERE ROOM_COST_PER_NIGHT = @ROOM_COST_PER_NIGHT
         `
       );
 
     res.status(201).json({
-      message: "TRANSACTION ADDED SUCCESSFULLY",
-      transactionId: transactionIDResult.recordset[0].TRANSACTION_ID,
-      bookingId: BOOKING_ID,
+      message: "ROOM ADDED SUCCESSFULLY",
+      roomId: roomIDResult.recordset[0].ROOM_ID
     });
   } catch (error) {
     res.status(500).json({ error: error.message });
@@ -118,23 +113,23 @@ router.delete("/:id", authenticateToken, authorizeUser([userTables.admin], false
     const id = parseInt(req.params.id);
 
     if (isNaN(id)) {
-      return res.status(400).json({ error: "INVALID TRANSACTION ID" });
+      return res.status(400).json({ error: "INVALID ROOM ID" });
     }
 
     const pool = await sql.connect(config);
     const result = await pool
       .request()
-      .input("TRANSACTION_ID", sql.Int, id)
-      .query(`DELETE FROM TRANSACTIONS WHERE TRANSACTION_ID = @TRANSACTION_ID`);
+      .input("ROOM_ID", sql.Int, id)
+      .query(`DELETE FROM ROOMS WHERE ROOM_ID = @ROOM_ID`);
 
     if (result.rowsAffected[0] === 0) {
       return res
         .status(404)
-        .json({ error: `TRANSACTION ${id} NOT FOUND` });
+        .json({ error: `ROOM ${id} NOT FOUND` });
     }
     res
       .status(200)
-      .json({ message: `TRANSACTION ${id} DELETED SUCCESSFULLY` });
+      .json({ message: `ROOM ${id} DELETED SUCCESSFULLY` });
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
@@ -143,8 +138,8 @@ router.delete("/:id", authenticateToken, authorizeUser([userTables.admin], false
 router.patch("/:id", authenticateToken, authorizeUser([userTables.admin], false), async (req, res) => {
   const id = parseInt(req.params.id);
   const updates = req.body;
-  const columnTypes = await fetchColumnTypes("TRANSACTIONS");
-  const columnNames = await fetchColumnNames("TRANSACTIONS");
+  const columnTypes = await fetchColumnTypes("ROOMS");
+  const columnNames = await fetchColumnNames("ROOMS");
 
   if (Object.keys(updates).length === 0) {
     return res.status(400).json({ error: "NO FIELDS TO UPDATE" });
@@ -159,34 +154,34 @@ router.patch("/:id", authenticateToken, authorizeUser([userTables.admin], false)
   const request = pool.request();
 
   for (let field in updates) {
-    if (field === "TRANSACTION_ID" || field === "BOOKING_ID") {
+    if (field === "ROOM_ID") {
       return res.status(403).json({ error: "FORBIDDEN" });
     }
-    if (field === "PAYMENT_STATUS" || field === "MODE_OF_PAYMENT") {
+    if (field === "ROOM_TYPE") {
       updates[field] = updates[field].toLowerCase();
     }
-    if (field === "DISCOUNT") {
+    if (field === "MAX_OCCUPANCY") {
       if (isNaN(updates[field])) {
         return res.status(400).json({ error: "INVALID NUMBER FORMAT" });
       }
-      updates[field] = parseInt(updates[field]);
+      updates[field] = parseFloat(updates[field]);
     }
     request.input(field, columnTypes[field], updates[field]);
     updateFields.push(`${field} = @${field}`);
   }
-  request.input("TRANSACTION_ID", sql.Int, id);
+  request.input("ROOM_ID", sql.Int, id);
 
-  const query = `UPDATE TRANSACTIONS SET ${updateFields.join(", ")} WHERE TRANSACTION_ID = @TRANSACTION_ID`;
+  const query = `UPDATE ROOMS SET ${updateFields.join(", ")} WHERE ROOM_ID = @ROOM_ID`;
 
   try {
     const result = await request.query(query);
     if (result.rowsAffected[0] === 0) {
-      return res.status(404).json({ error: "TRANSACTION NOT FOUND" });
+      return res.status(404).json({ error: "ROOM NOT FOUND" });
     }
 
     res
       .status(200)
-      .json({ message: `TRANSACTION ${id} UPDATED SUCCESSFULLY` });
+      .json({ message: `ROOM ${id} UPDATED SUCCESSFULLY` });
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
